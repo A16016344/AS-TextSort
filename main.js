be5c8f254ca4579dd017537e7d4d4b1036f18e3e
A16016344 <a16016344@alumnos.uady.mx>
2020-10-01 11:08:28 -0500

    Mejoras menores

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 botonLeerTxt = document.getElementById("leerTxt")
 txtUrl = document.getElementById("txtUrl")
 botonOrdenarLista = document.getElementById("ordenarLista")
+inputArchivo = document.getElementById('file-input')
 
 botonLeerTxt.addEventListener("click", function(e){
     e.preventDefault() //Evitamos que el form recargue la pagina
     leerTxt(txtUrl.value,'resultado'); //Capturamos la URL del input y la pasamos al card
 })
 
+inputArchivo.addEventListener('change', leerArchivo, false);
+
 botonOrdenarLista.addEventListener("click", function(){
     var aux = divToVar('resultado') //tomamos el valor de la card y lo guardamos en un arreglo
-    ordenarLista(aux) // Ordenamos alfabeticamente el arreglo
     for(i = 0; i < aux.length; i++){
         aux[i] = aux[i].split(" ") //Dividimos cada nombre en palabras separadas por espacio en una matriz
         for(j = 0; j < aux[i].length; j++){
@@ -17,6 +19,7 @@ botonOrdenarLista.addEventListener("click", function(){
         }
         aux[i] = aux[i].join(" ") //Volvemos a juntar todas las palabras de cada nombre
     }
+    ordenarLista(aux) // Ordenamos alfabeticamente el arreglo
     arrayToDiv(aux,'resultado') //Imprimimos el arreglo ordenado y capitalizado en la card
 })
 
@@ -29,6 +32,26 @@ async function leerTxt(url,id) {
     } catch (err) {
       console.error(err);
     }
+    document.getElementById("nombre-archivo").innerText = url+` :`
+    document.getElementById("input-card").innerHTML = `<a class="btn btn-danger" href="javascript:location.reload()">Elegir otro archivo</a>` 
+    document.getElementById("card-resultado").classList.remove("invisible")
+}
+
+// Lee un archivo local desde un input file e imprime en un elemento html
+  function leerArchivo(e) {
+    var archivo = e.target.files[0];
+    if (!archivo) {
+      return;
+    }
+    var lector = new FileReader();
+    lector.onload = function(e) {
+      var data = e.target.result;
+      document.getElementById('resultado').innerText = data
+    };
+    lector.readAsText(archivo);
+    document.getElementById("nombre-archivo").innerText = e.target.files[0].name+` :`
+    document.getElementById("input-card").innerHTML = `<a class="btn btn-danger" href="javascript:location.reload()">Elegir otro archivo</a>` 
+    document.getElementById("card-resultado").classList.remove("invisible")
   }
 
 // Guarda en un arrglo cada linea del texto de un elemento del html
